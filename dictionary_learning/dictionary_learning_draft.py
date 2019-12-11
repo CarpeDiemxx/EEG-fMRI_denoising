6733ec7c0984cd3d6f4c5ad19ac342b106f9f820
Bohui Zhang <bhzhang97@gmail.com>
2019-12-11 20:49:08 +0800

    Draft version for debugging

--- dictionary_learning/dictionary_learning_draft.py
+++ dictionary_learning/dictionary_learning_draft.py
@@ -1,45 +1,29 @@
-<<<<<<< HEAD
 # -*- coding: utf-8 -*-
 """
 Created on Mon Oct 11 2019
 
 @author: Bohui Zhang
-=======
 """
->>>>>>> 2a7585c176806692418b800359f60238ba7dd104
-"""
-
 import random
 import numpy as np
 import pandas as pd
 from scipy.io import loadmat
 
 # global variables
-<<<<<<< HEAD
 eeg_data_length = 1200
 segment_length = 50
 segment_number = 100 
-=======
-eeg_data_length = 10000
-segment_length = 50
-segment_number = 2000 
->>>>>>> 2a7585c176806692418b800359f60238ba7dd104
 
 # load EEG data
 # train_data.mat -> x -> (1, 1200)
 # eeg_data.mat -> EEG_data -> (32, 332416)
-<<<<<<< HEAD
 data_set = 3
-=======
-data_set = 2
->>>>>>> 2a7585c176806692418b800359f60238ba7dd104
 print('Loading data...')
 if data_set == 1:
 	train_data_mat = loadmat('train_data.mat')
 	# print(train_data_mat.keys())
 	train_data = train_data_mat['x'][0, :eeg_data_length]
 	# print(train_data, train_data.shape)
-<<<<<<< HEAD
 elif data_set == 2:
 	train_data_mat = loadmat('eeg_data.mat')
 	train_data = train_data_mat['EEG_data'].sum(axis=0).reshape(1, 332416)
@@ -47,13 +31,6 @@ elif data_set == 2:
 else:
 	train_data_mat = loadmat('SimuEEG.mat') 
 	train_data = train_data_mat['x'][0, :eeg_data_length]
-
-=======
-else:
-	train_data_mat = loadmat('eeg_data.mat')
-	train_data = train_data_mat['EEG_data'].sum(axis=0).reshape(1, 332416)
-	train_data = train_data[0, 20000:20000+eeg_data_length]
->>>>>>> 2a7585c176806692418b800359f60238ba7dd104
 
 def seg_extract(x:'original EEG data', m:'segment number', n:'segment length'):
 	"""
@@ -78,17 +55,7 @@ def seg_extract(x:'original EEG data', m:'segment number', n:'segment length'):
 				R[i][k][j+k] = 1
 		x_seg[:, i] = (R[i].dot(x.T)).reshape(50)
 		# move back the index randomly to implement overlapping
-<<<<<<< HEAD
 		t = random.randint(n*2/5, n)
-		j = j + n - t
-	return x_seg, R
-
-def dict_initialize(x_seg:'segmented EEG data', n_components:'dimension of dictionary'):
-	"""
-	Initialize the dictionary by taking the first n_components column vectors of 
-	left sigular matrix of original EEG data as atoms of the initial dictionary.
-=======
-		t = random.randint(n*1/5, n)
 		j = j + n - t
 	return x_seg, R
 
@@ -97,19 +64,16 @@ def dict_initialize(x_seg:'segmented EEG data', n_components:'dimension of dicti
 	Initialize the dictionary by taking the first n_components column vectors of 
 	left sigular matrix of original EEG data as atoms of the initial dictionary.
 
->>>>>>> 2a7585c176806692418b800359f60238ba7dd104
 	Returns:
 		dict_data: initialized dictionary (dict_data.shape = (n, n_components))
 	"""
 	u, sigma, v = np.linalg.svd(x_seg)
-<<<<<<< HEAD
 	print('x_seg.shape =', x_seg.shape)
 	print('u.shape =', u.shape) 
 	dict_data = u[:, :n_components]  # dict_data.shape = (50, 50) ! 
 	print('dict_shape =', dict_data.shape)
-=======
 	dict_data = u[:, :n_components]
->>>>>>> 2a7585c176806692418b800359f60238ba7dd104
+
 	return dict_data
 
 def dict_update(x:'data', D:'dictionary', s, n_components):
@@ -134,11 +98,8 @@ def dict_update(x:'data', D:'dictionary', s, n_components):
 			s[i, k] = sigma[0] * v[0, j]
 	return D, s
 
-<<<<<<< HEAD
-def x_estimate(y:'original EEG data', D, s, m, n, const_lambda=0.01):
-=======
 def x_estimate(y:'original EEG data', D, s, m, n, const_lambda=0.5):
->>>>>>> 2a7585c176806692418b800359f60238ba7dd104
+
 	"""
 	Estimate x (BCG artifact) using Eq.(5) in (Abolghasemi et al., 2015).
 	"""
@@ -157,11 +118,11 @@ from sklearn import linear_model
 y = train_data.reshape(1, eeg_data_length).T
 print('Extracting segments...')
 x_seg, R = seg_extract(train_data, m=segment_number, n=segment_length)
-<<<<<<< HEAD
+
 dictionary = dict_initialize(x_seg, segment_number)
-=======
+
 dictionary = dict_initialize(x_seg)
->>>>>>> 2a7585c176806692418b800359f60238ba7dd104
+
 max_iter = 5
 tolerance = 0.15
 
@@ -189,11 +150,10 @@ scio.savemat('v.mat', {'v':v})
 
 np.save('y.npy', y)
 np.save('v.npy', v)
-<<<<<<< HEAD
+
 np.save('x.npy', x)
 np.save('dictionary.npy', dictionary) 
-=======
->>>>>>> 2a7585c176806692418b800359f60238ba7dd104
+
 
 import matplotlib.pyplot as plt 
 
@@ -205,7 +165,7 @@ def eeg_plot(y:'original signal', v:'clean EEG', x:'BCG'):
 	plt.figure(figsize=(5, 20))
 	# plt.ylim(-1.5, 1.5)
 	plt.plot(t, y, color='b')
-<<<<<<< HEAD
+
 	#plt.plot(t, v, color='r')
 	plt.plot(t, x, color='r')
 	plt.show()
@@ -225,11 +185,5 @@ print('s.shape =', s.shape)
 
 #eeg_plot(real_EEG, v, x)
 eeg_plot(y, v, x)
-=======
-	plt.plot(t, v, color='r')
-	# plt.plot(t, x, color='c')
-	plt.show()
 
-print('Plotting...')
-eeg_plot(y, v, x)
->>>>>>> 2a7585c176806692418b800359f60238ba7dd104
+
