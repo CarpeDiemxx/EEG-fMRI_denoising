a65a1f07919eee4130825e893af550a53bb2f0b3
Bohui Zhang <bhzhang97@gmail.com>
2020-03-11 23:35:46 -0700

    Atom analysis (unused standards)

--- dictionary_learning/atom_selection.py
+++ dictionary_learning/atom_selection.py
@@ -2,7 +2,7 @@ import random
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt 
-
+import math
 
 D = np.load('results\\dictionary.npy')
 x = np.load('results\\x.npy')
@@ -13,43 +13,42 @@ def correlation_coefficient(x, y):
 	var_y = np.sqrt(((y - y.mean()) ** 2).sum())
 	return abs(numerator / (var_x * var_y))
 
-def corr_coef(x, y):
-	return x.corr(y)
-
 def correlation_coefficient_avg(D, x):
 	corr_list = np.zeros((D.shape[1], 1), dtype=float)
 	corr_list_avg = np.zeros((D.shape[1], 1), dtype=float)
-	for test_times in range(1, 501):
+	for test_times in range(1, 5):
 		start_point = random.randint(0, x.shape[0]-D.shape[1])
 		end_point = start_point + D.shape[1]
-		x_segs = x[start_point:end_point, 0]
+		x_segs = x[start_point:end_point, ]
 		for i in range(0, D.shape[1]):
 			corr_list[i, 0] = correlation_coefficient(x_segs, D[:, i])
 		corr_list_avg += corr_list
 		corr_list_avg = corr_list_avg / test_times
 
-	x_axis = np.linspace(1, 50, 50) 
+	x_axis = np.linspace(1, D.shape[1], D.shape[1]) 
 	plt.scatter(x_axis, corr_list_avg, marker = 'o', color = 'c', s = 50)
 	plt.plot(x_axis, np.zeros((D.shape[1], 1)), color = 'grey')
 	plt.show()
 
+#correlation_coefficient_avg(D, D[:, 50])
+
 from matplotlib.collections import LineCollection
 from matplotlib.ticker import MultipleLocator
 
-def plot_eeg(data):
+def plot_dictionary(data):
 	# Plot the EEG
-	n_samples = 50
-	t = np.arange(n_samples)
+	n_components = data.shape[0]
+	t = np.arange(n_components)
 	ticklocs = []
 	ax2 = plt.figure('D').add_subplot(1, 1, 1)
 	print(type(ax2))
-	ax2.set_xlim(0, 50)
-	ax2.set_xticks(np.arange(50))
+	ax2.set_xlim(0, n_components)
+	ax2.set_xticks(np.arange(n_components))
 	dmin = data.min()
 	dmax = data.max()
 	dr = (dmax - dmin) * 0.7  # Crowd them a bit.
 	y0 = dmin
-	n_rows = 50
+	n_rows = n_components
 	y1 = (n_rows - 1) * dr + dmax
 	ax2.set_ylim(y0, y1)
 	segs = []
@@ -63,17 +62,17 @@ def plot_eeg(data):
 	# Set the yticks to use axes coordinates on the y axis
 	ax2.set_yticks(ticklocs)
 	y_list = []
-	for i in range(1, 51):
+	for i in range(1, n_components+1):
 		y_list.append('%d' % i)
 	ax2.set_yticklabels(y_list)
 	ax2.set_xlabel('Time (s)')
 	plt.tight_layout()
 	plt.show()
 
-#plot_eeg(D)
+#plot_dictionary(D)
 
 # clustering
-from sklearn.decomposition import PCA
+
 def dictionary_pca(D):
 	mean = np.mean(D, axis=0)
 	X = D - mean
@@ -89,10 +88,11 @@ def dictionary_pca(D):
 	plt.ylabel(r'$\bar {\lambda}_{l}$', fontsize=18, color='black', horizontalalignment='center',
 		 	   fontname='Times New Roman')  
 	plt.plot(xx, S[xx-1], linestyle="-", marker="o", color="b", linewidth=2)
-	#plt.savefig('fig4.png')
 	plt.show()
 
-def atom_clustering_1(D):
+#dictionary_pca(D)
+
+def mean_clustering(D):
 	"""
 	Based on mean.
 	"""
@@ -105,25 +105,19 @@ def atom_clustering_1(D):
 	plt.scatter(x_axis, atoms_mean_list)
 	plt.show()
 
-def atom_clustering_2(D):
+def var_clustering(D):
 	"""
+	Based on std var, log2(std). 
 	"""
-	atom_number = D.shape[0]
-	atoms_mean_list = np.mean(D, 0)
-	diff_sum_list = abs(D - atoms_mean_list).sum(axis=0)
-	print(diff_sum_list)
+	atoms_number = D.shape[1]
+	std_list = list(map(lambda x: np.std(x, ddof=1), (D[:, i] for i in range(atoms_number))))
+	std_log_list = list(map(lambda x: -1 * math.log2(x), std_list))
+	print(std_log_list)
 	plt.figure(figsize=(7, 5))
-	x_axis = np.linspace(1, atom_number, atom_number) 
-	plt.scatter(x_axis, diff_sum_list, color='b')
+	x_axis = np.linspace(1, atoms_number, atoms_number) 
+	plt.scatter(x_axis, std_log_list, color='b')
 	plt.show()
 
-def dictionary_modification(D):
-	U, S, V = np.linalg.svd(D) 
-	print(S)
-	x = np.linspace(1, S.shape[0], S.shape[0])
-	plt.scatter(x, S) 
-	plt.show()
+#var_clustering(D)
 
-def 
 
-atom_clustering_2(D)
